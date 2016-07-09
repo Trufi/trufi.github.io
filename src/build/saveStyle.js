233518b6caeb8e3a89386dff80321cd39f06871d
Mstislav Zhivodkov <stevemyz@gmail.com>
2016-07-09 20:15:25 +0600

    Remove old short folder and add csso

--- src/build/saveStyle.js
+++ src/build/saveStyle.js
@@ -1,8 +1,12 @@
+'use strict';
+
 var path = require('path');
+var csso = require('csso');
 var fs = require('fs');
 
 module.exports = function(config) {
     var style = fs.readFileSync(path.join(__dirname, '../style.css'), 'utf8');
+    var compressedStyle = csso.minify(style).css;
 
-    fs.writeFileSync(path.join(config.distPath, 'style.css'), style, 'utf8');
+    fs.writeFileSync(path.join(config.distPath, 'style.css'), compressedStyle, 'utf8');
 };
